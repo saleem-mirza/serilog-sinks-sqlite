28d618c1907c38d1a96c1573e4635154c7eadf76
Saleem Mirza <mmirza@gategourmet.com>
2016-09-12 00:00:20 -0400

    Added skeleton files

---
+++ src/serilog-sinks-sqlite/LoggerConfigurationSQLiteExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Adds the WriteTo.SQLite() extension method to <see cref="LoggerConfiguration"/>.
+    /// </summary>
+    public static class LoggerConfigurationSQLiteExtensions
+    {
+    }
+}
---
+++ src/serilog-sinks-sqlite/Sinks/SQLite/SQLiteSink.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Sinks.SQLite
+{
+    public class SQLiteSink : ILogEventSink, IDisposable
+    {
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+   
+        #endregion
+
+
+        public void Emit(LogEvent logEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
