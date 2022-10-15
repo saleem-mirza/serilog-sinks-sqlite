878c1fbb39af8d3cf440575e5a4a3684241e09e5
Stephen Poland <stephen.poland@outlook.com>
2022-10-15 11:16:35 +0100

    Render the message before inserting to database.

--- src/Serilog.Sinks.SQLite/Sinks/SQLite/SQLiteSink.cs
+++ src/Serilog.Sinks.SQLite/Sinks/SQLite/SQLiteSink.cs
@@ -275,7 +275,7 @@ namespace Serilog.Sinks.SQLite
                         sqlCommand.Parameters["@level"].Value = logEvent.Level.ToString();
                         sqlCommand.Parameters["@exception"].Value =
                             logEvent.Exception?.ToString() ?? string.Empty;
-                        sqlCommand.Parameters["@renderedMessage"].Value = logEvent.MessageTemplate.ToString();
+                        sqlCommand.Parameters["@renderedMessage"].Value = logEvent.MessageTemplate.Render(logEvent.Properties, _formatProvider);
 
                         sqlCommand.Parameters["@properties"].Value = logEvent.Properties.Count > 0
                             ? logEvent.Properties.Json()
